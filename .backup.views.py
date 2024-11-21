59dabfbc65e3947d281279a4d04c5dff53b27299
Robocoders <team@blackbox.ai>
2024-11-21 10:59:52 +0000

    Update

--- .backup.views.py
+++ .backup.views.py
@@ -1,7 +1,6 @@
 
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
-from django.contrib.auth.models import User
 from django.contrib.sites.shortcuts import get_current_site
 from django.template.loader import render_to_string
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
