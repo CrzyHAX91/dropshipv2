3f35242c5a2aeabacf8872981efc3f04ffce6fae
Robocoders <team@blackbox.ai>
2024-11-21 10:57:41 +0000

    Update

--- email_utils.py
+++ email_utils.py
@@ -1,55 +1,33 @@
 
 from django.core.mail import send_mail
 from django.conf import settings
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
 
 def send_order_confirmation_email(order):
     subject = f'Order Confirmation - Order #{order.id}'
-    message = f'''
-    Dear {order.user.username},
-
-    Thank you for your order. Your order details are as follows:
-
-    Order ID: {order.id}
-    Total Amount: ${order.total_price}
-    Status: {order.get_status_display()}
-
-    We will notify you when your order has been shipped.
-
-    Best regards,
-    Your Dropshipping Team
-    '''
+    html_message = render_to_string('emails/order_confirmation.html', {'order': order})
+    plain_message = strip_tags(html_message)
+    
     send_mail(
         subject,
-        message,
+        plain_message,
         settings.DEFAULT_FROM_EMAIL,
         [order.user.email],
+        html_message=html_message,
         fail_silently=False,
     )
 
 def send_order_status_update_email(order):
     subject = f'Order Status Update - Order #{order.id}'
-    message = f'''
-    Dear {order.user.username},
-
-    Your order status has been updated:
-
-    Order ID: {order.id}
-    New Status: {order.get_status_display()}
-
-    {
-    "Your order has been shipped." if order.status == 'shipped' else
-    "Your order is being processed." if order.status == 'processing' else
-    "Your order has been delivered." if order.status == 'delivered' else
-    "There has been an update to your order."
-    }
-
-    Best regards,
-    Your Dropshipping Team
-    '''
+    html_message = render_to_string('emails/order_status_update.html', {'order': order})
+    plain_message = strip_tags(html_message)
+    
     send_mail(
         subject,
-        message,
+        plain_message,
         settings.DEFAULT_FROM_EMAIL,
         [order.user.email],
+        html_message=html_message,
         fail_silently=False,
     )
