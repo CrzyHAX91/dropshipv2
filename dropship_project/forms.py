d428f2badfd649a4ded648233268d4b2ea2f493a
Your Name <your-email@example.com>
2024-11-21 15:21:48 +0000

    Update

--- dropship_project/forms.py
+++ dropship_project/forms.py
@@ -1,49 +1,35 @@
 from django import forms
-from django.contrib.auth.forms import PasswordChangeForm, UserCreationForm, AuthenticationForm
-from django.contrib.auth import password_validation
-from captcha.fields import ReCaptchaField
-from captcha.widgets import ReCaptchaV2Checkbox
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
 from .models import CustomUser
+
+class UserRegistrationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'email', 'password1', 'password2')
+
+    def clean_password1(self):
+        password1 = self.cleaned_data.get('password1')
+        try:
+            validate_password(password1, self.instance)
+        except ValidationError as error:
+            self.add_error('password1', error)
+        return password1
 
 class CustomAuthenticationForm(AuthenticationForm):
     remember_me = forms.BooleanField(required=False, initial=False)
-    captcha = ReCaptchaField(widget=ReCaptchaV2Checkbox)
-
-class CustomPasswordChangeForm(PasswordChangeForm):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['old_password'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Current Password'})
-        self.fields['new_password1'].widget.attrs.update({'class': 'form-control', 'placeholder': 'New Password'})
-        self.fields['new_password2'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Confirm New Password'})
-
-    def clean_new_password1(self):
-        password1 = self.cleaned_data.get('new_password1')
-        try:
-            password_validation.validate_password(password1, self.user)
-        except forms.ValidationError as error:
-            self.add_error('new_password1', error)
-        return password1
 
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = CustomUser
-        fields = ['username', 'email', 'first_name', 'last_name', 'phone_number', 'address', 'date_of_birth']
-        widgets = {
-            'date_of_birth': forms.DateInput(attrs={'type': 'date'}),
-        }
+        fields = ('username', 'email', 'first_name', 'last_name')
 
-class UserRegistrationForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-    captcha = ReCaptchaField(widget=ReCaptchaV2Checkbox)
-
-    class Meta:
-        model = CustomUser
-        fields = ('username', 'email', 'password1', 'password2', 'captcha')
-
-    def save(self, commit=True):
-        user = super(UserRegistrationForm, self).save(commit=False)
-        user.email = self.cleaned_data['email']
-        if commit:
-            user.save()
-        return user
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if CustomUser.objects.filter(email=email).exclude(pk=self.instance.pk).exists():
+            raise forms.ValidationError('This email address is already in use.')
+        return email
 
