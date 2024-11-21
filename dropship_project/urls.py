2f9f3187931b88ecf6ca253256c2faf750af7279
Your Name <your-email@example.com>
2024-11-21 15:29:28 +0000

    Update

--- dropship_project/urls.py
+++ dropship_project/urls.py
@@ -1,25 +1,15 @@
+from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth import views as auth_views
-from rest_framework.routers import DefaultRouter
-from two_factor.urls import urlpatterns as tf_urls
-from .views import ProductViewSet, OrderViewSet, register, activate_account, RateLimitedPasswordResetView, user_profile
-from .admin import admin_site
-
-router = DefaultRouter()
-router.register(r'products', ProductViewSet)
-router.register(r'orders', OrderViewSet)
+from django.views.generic import TemplateView
+from .views import CustomLoginView, register, activate_account, user_profile
 
 urlpatterns = [
-    path('', include(tf_urls)),
-    path('admin/', admin_site.urls),
-    path('', include(router.urls)),
-    path('register/', register, name='register'),
-    path('activate/<str:uidb64>/<str:token>/', activate_account, name='activate_account'),
-    path('password_reset/', RateLimitedPasswordResetView.as_view(), name='password_reset'),
-    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
-    path('profile/', user_profile, name='user_profile'),
+    path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('accounts/', include('allauth.urls')),
+    path('login/', CustomLoginView.as_view(), name='account_login'),
+    path('register/', register, name='account_signup'),
+    path('activate/<str:uidb64>/<str:token>/', activate_account, name='activate_account'),
+    path('profile/', user_profile, name='user_profile'),
 ]
 
