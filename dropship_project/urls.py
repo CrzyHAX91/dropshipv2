b2ee11a4cc587a950f2f9a41c16e702e0f59d676
Your Name <your-email@example.com>
2024-11-21 15:08:00 +0000

    Update

--- dropship_project/urls.py
+++ dropship_project/urls.py
@@ -1,17 +1,16 @@
-
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
-from django.contrib import admin
 from rest_framework.routers import DefaultRouter
 from two_factor.urls import urlpatterns as tf_urls
 from .views import ProductViewSet, OrderViewSet, register, activate_account, RateLimitedPasswordResetView, user_profile
+from .admin import admin_site
 
 router = DefaultRouter()
 router.register(r'products', ProductViewSet)
 router.register(r'orders', OrderViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin_site.urls),
     path('', include(router.urls)),
     path('', include(tf_urls)),
     path('register/', register, name='register'),
@@ -22,5 +21,5 @@ urlpatterns = [
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     path('profile/', user_profile, name='user_profile'),
     path('accounts/', include('allauth.urls')),
-    path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
 ]
+
