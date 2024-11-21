481aeddcb63ad93d071034c20d471e48d15e186f
Your Name <your-email@example.com>
2024-11-21 15:55:59 +0000

    Update

--- dropship_project/admin_widgets.py
+++ dropship_project/admin_widgets.py
@@ -1,13 +1,14 @@
 from django.utils.translation import gettext_lazy as _
 from django.urls import reverse
 from django.utils.html import format_html
-from admin_tools.dashboard import modules
+from django.contrib.admin.widgets import AdminDateWidget
+from django.forms.widgets import Media, MEDIA_TYPES
 
-class QuickLinksWidget(modules.LinkList):
+class QuickLinksWidget:
     title = _('Quick Links')
+    template = 'admin/widgets/quick_links.html'
 
-    def __init__(self, **kwargs):
-        super(QuickLinksWidget, self).__init__(**kwargs)
+    def __init__(self):
         self.children = [
             {
                 'title': _('Add New Product'),
@@ -23,23 +24,24 @@ class QuickLinksWidget(modules.LinkList):
             },
         ]
 
-class RecentOrdersWidget(modules.DashboardModule):
+class RecentOrdersWidget:
     title = _('Recent Orders')
     template = 'admin/widgets/recent_orders.html'
 
-    def __init__(self, **kwargs):
-        super(RecentOrdersWidget, self).__init__(**kwargs)
+    def __init__(self):
         self.limit = 5
 
-    def init_with_context(self, context):
+    def get_context(self):
         from .models import Order
-        self.children = Order.objects.order_by('-created_at')[:self.limit]
+        return {
+            'orders': Order.objects.order_by('-created_at')[:self.limit]
+        }
 
-class SalesStatisticsWidget(modules.DashboardModule):
+class SalesStatisticsWidget:
     title = _('Sales Statistics')
     template = 'admin/widgets/sales_statistics.html'
 
-    def init_with_context(self, context):
+    def get_context(self):
         from .models import Order
         from django.db.models import Sum
         from django.utils import timezone
@@ -47,7 +49,9 @@ class SalesStatisticsWidget(modules.DashboardModule):
         today = timezone.now().date()
         this_month = today.replace(day=1)
 
-        self.total_sales = Order.objects.aggregate(total=Sum('total_price'))['total'] or 0
-        self.today_sales = Order.objects.filter(created_at__date=today).aggregate(total=Sum('total_price'))['total'] or 0
-        self.month_sales = Order.objects.filter(created_at__date__gte=this_month).aggregate(total=Sum('total_price'))['total'] or 0
+        return {
+            'total_sales': Order.objects.aggregate(total=Sum('total_price'))['total'] or 0,
+            'today_sales': Order.objects.filter(created_at__date=today).aggregate(total=Sum('total_price'))['total'] or 0,
+            'month_sales': Order.objects.filter(created_at__date__gte=this_month).aggregate(total=Sum('total_price'))['total'] or 0,
+        }
 
